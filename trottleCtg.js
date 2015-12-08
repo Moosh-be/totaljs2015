a96eb769ec62b8f829239e3add0f68b64acac542
Moosh-be <ctg@rtbf.be>
2015-12-08 15:01:41 +0100

    corrigé

--- trottleCtg.js
+++ trottleCtg.js
@@ -1,14 +1,14 @@
 Function.prototype.throttle = function(duration) {
-  var f = this;
-  
-  var start = new Date.now() ;
-  var end = start + duration;
+	var f = this, end = 0;
 
-  return function() {
-  	var now = new Date.now();
-	if (end < now)     
-    return f.apply(this, arguments);
-  };
+	return function() {
+		var now = Date.now();
+		if (end < now) {
+			end = now + duration;
+			return f.apply(this, arguments);
+		}
+		return;
+	};
 };
 
 // Protocole de test
@@ -18,4 +18,4 @@ function sayHi() { console.log(Date.now(), "Hiiiii…"); }
 console.log(Date.now());
 hiCoquine = setInterval(sayHi.throttle(1000), 100);
 
-setTimeout(function() { clearInterval(hiCoquine); }, 10000);
+setTimeout(function() { clearInterval(hiCoquine); }, 10000);					
