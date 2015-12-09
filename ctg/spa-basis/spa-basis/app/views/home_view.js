b9c78f2764b381e71215ffa94d3970d62c6db150
Moosh-be <ctg@rtbf.be>
2015-12-09 17:11:34 +0100

    gps

--- ctg/spa-basis/spa-basis/app/views/home_view.js
+++ ctg/spa-basis/spa-basis/app/views/home_view.js
@@ -1,18 +1,15 @@
-// Contrôleur principal
-// ====================
-
 'use strict';
 
 var View = require('./view');
 var userName = require('lib/notifications').userName;
 var moment = require('moment');
+var CheckInView = require('./check_in_view');
 
 module.exports = View.extend({
 	// Le template principal
 	template: require('./templates/home'),
 
 	getRenderData: function() {
-		console.log(userName);
 		return {
 			userName: userName,
 			now: moment().format('dddd D MMMM YYYY HH:mm:ss')
@@ -21,6 +18,9 @@ module.exports = View.extend({
 
 	afterRender: function afterHomeRender() {
 		this.startClock();
+		new CheckInView({
+			el: this.$('#checkInUI')
+		}).render();
 	},
 
 	startClock: function() {
