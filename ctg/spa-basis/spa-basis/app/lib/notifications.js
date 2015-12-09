e9889aaa79fee70de8355e8133be654741586bd4
Moosh-be <ctg@rtbf.be>
2015-12-09 15:09:55 +0100

    prompt et save sessionsto

--- ctg/spa-basis/spa-basis/app/lib/notifications.js
+++ ctg/spa-basis/spa-basis/app/lib/notifications.js
@@ -1,1 +1,15 @@
-'use stricts';
+'use strict';
+
+var $ = require('jquery');
+
+var userName = sessionStorage.userName || $.trim(
+	prompt("Votre nom ?")
+);
+
+if (userName) {
+	sessionStorage.userName = userName;
+} else {
+	userName = 'Anon';
+}
+
+exports.userName = userName;
--- ctg/spa-basis/spa-basis/app/views/home_view.js
+++ ctg/spa-basis/spa-basis/app/views/home_view.js
@@ -4,8 +4,17 @@
 'use strict';
 
 var View = require('./view');
+var userName = require('lib/notifications').userName;
+
 
 module.exports = View.extend({
-  // Le template principal
-  template: require('./templates/home')
+	// Le template principal
+	template: require('./templates/home'),
+
+	getRenderData: function() {
+		console.log(userName);
+		return {
+			userName: userName
+		};
+	}
 });
