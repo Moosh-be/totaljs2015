cbd1e798692e7448b7f9c0959aa94138de56dfb8
Mathieu Laurent <mathieu.laurent@gmail.com>
2015-12-10 10:46:59 +0100

    Add view event, model modification

--- mla/spa-basis/app/models/check_in_ux.js
+++ mla/spa-basis/app/models/check_in_ux.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Backbone = require('backbone');
+var _ = require('underscore');
 
 // Bon, on n'a *rien* à rajouter aux capacités inhérentes
 // de Backbone.Model, mais c'est toujours mieux de prévoir un
@@ -8,10 +9,29 @@ var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
 	defaults: {
-		lat:0,
-		lng:0,
+		lat: 0,
+		lng: 0,
 		places: [],
 		placeId: null,
-		comment:''
+		comment: '',
+		checkInForbidden: true
+	},
+	initialize: function() {
+		this.on('change', checkCheckInAvailable.bind(this));
+		checkCheckInAvailable.call(this);
+
+		function checkCheckInAvailable() {
+			return this.set('checkInForbidden', (this.get('placeId') === null) ? true : false);
+		}
+
+	},
+	getPlace : function () {
+
+		var place =  _.findWhere(
+				this.get('places'), {
+					id: this.get('placeId')
+				}
+			);
+		return place;
 	}
 });
--- mla/spa-basis/app/views/check_in_views.js
+++ mla/spa-basis/app/views/check_in_views.js
@@ -5,6 +5,7 @@ var locSvc = require('lib/location');
 var poiSvc = require('lib/places');
 var CheckInUx = require('models/check_in_ux');
 var _ = require('underscore');
+var userName = require('lib/notifications').userName;
 
 module.exports = View.extend({
 	bindings: {
@@ -22,13 +23,51 @@ module.exports = View.extend({
 			updateMethod: 'html'
 		},
 		'#places': {
-			observe: ['places'],
+			observe: ['places', 'placeId'],
 			onGet: function() {
 				return this.getRenderData().placeList;
 			},
 			updateMethod: 'html'
 		},
-		// …
+		// http://nytimes.github.io/backbone.stickit/#attributes
+		'button[type=submit]': {
+			attributes: [{
+				name: 'disabled',
+				observe: 'checkInForbidden'
+			}]
+		},
+	},
+	events: {
+		// La clé est composée du nom de l'événement et du sélecteur CSS.
+		// La valeur est le nom de la méthode du controler
+		'click .btn-info': 'fetchPlaces',
+		'click #places li': 'selectPlace',
+		'submit': 'checkIn'
+	},
+	checkIn: function(e) {
+		e.preventDefault();
+		if (this.model.get('checkInForbidden')) {
+			return true;
+		}
+		var place = this.model.getPlace();
+		var checkIn = {
+			placeId: place.id,
+			vicinity : place.vicinity,
+			icon : place.icon,
+			name : place.name,
+			comment : this.model.get('comment'),
+			userName : userName
+		};
+		this.model.set({
+			comment : this.model.defaults.comment,
+			placeId : this.model.defaults.placeId
+		});
+	},
+	selectPlace: function(e) {
+		// console.log(e.currentTarget, e.target, this);
+		var elt = this.$(e.currentTarget);
+		var placeId = elt.data('place-id');
+		this.model.set('placeId', placeId);
 	},
 	// Le template principal
 	template: require('./templates/check_in'),
@@ -40,7 +79,8 @@ module.exports = View.extend({
 	getRenderData: function() {
 		return {
 			placeList: this.renderTemplate({
-				places: this.model.get('places')
+				places: this.model.get('places'),
+				placeId: this.model.get('placeId')
 			}, this.placesTemplate)
 		};
 	},
@@ -49,6 +89,7 @@ module.exports = View.extend({
 	},
 	fetchPlaces: function() {
 		var that = this;
+		that.model.set(that.model.defaults);
 		locSvc.getCurrentLocation(function(lat, lng) {
 			if (_.isString(lat)) {
 				return;
